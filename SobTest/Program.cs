28bc9dc256eaffadd5f27334534f69a8021fca8f
FessTheDemon <nik311vas@gmail.com>
2021-04-21 16:50:36 +0300

    Some micro fixes

--- SobTest/Program.cs
+++ SobTest/Program.cs
@@ -14,7 +14,7 @@ namespace SobTest
         static void Main(string[] args)
         {
             double r;
-            Console.WriteLine("Введите стороны треугольника: ");
+            Console.WriteLine("Введите стороны треугольника через пробел: ");
             List<Double> numbers = Console.ReadLine().Split().Select(Double.Parse).ToList();
 
             Triangle three = new Triangle(numbers[0], numbers[1], numbers[2]);
